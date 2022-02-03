8b00170513a0e1879bd7381728ccde546079c318
Parra Rodriguez Manuel <parrarodriguez.manue@osdbsesto>
2022-02-03 10:36:46 +0100

    developed backend from previous repo

---
+++ backend/index.js
@@ -0,0 +1,63 @@
+const { request, response } = require("express");
+var express = require("express");
+var apiServer = express();
+var fs = require("fs");
+
+var port = 3000;
+var host = "localhost";
+apiServer.listen(port, host, () => {
+    console.log("Server running at http://%s:%d", host, port);
+});
+
+apiServer.get("/", (request, response)=>{
+    response.send("Ciao client sei in home");
+});
+
+apiServer.get("/login", (request, response)=>{
+    fs.readFile("users.json", (err, data)=>{
+        if(err){
+            response.send("error: ", err);
+        } else {
+            var users = JSON.parse(data);
+            var t1 = users.find(x => x.user==request.query.user);
+            var t2 = users.find(x => x.pswd==request.query.pswd);
+            if(t1==t2){
+                response.send("<h3>Checked</h3>");
+            } else {
+                response.send("<h3>Wrong crdentials</h3>");
+            }
+        }
+    })
+});
+
+apiServer.get("/addUser",  (request, response)=>{
+    var user = request.query.user;
+    var pswd = request.query.pswd;
+    fs.readFile("users.json", (err, data) => {
+        if(err){
+            console.log("Errore "+ err);
+            response.send("<body>ERROR<br>Cannot read user</body>");
+        } else {
+            var users = JSON.parse(data);
+            if(students.find(x => x.user==user)!== undefined){
+                var newUser = {
+                    "user": user,
+                    "pswd": pswd,
+                }
+                users.push(newUser);
+                fs.writeFile("users.json", JSON.stringify(users), (err) =>{
+                    if(err){
+                        console.log("Errore "+ err);
+                        response.send("<body>ERROR<br>Cannot save user</body>");
+                    } else {
+                        console.log(students);
+                        response.send("<h3>Registered new user</h3>");
+                    }
+                });
+            } else {
+                response.send("<h3>ERROR<br>User already exists</h3>");
+            }
+        }
+    });
+    fs.close(2);
+});
