c9d109d74602cdb3fed64ac33cd4d3b94527d7e6
Parra Rodriguez Manuel <parrarodriguez.manue@osdbsesto>
2022-02-03 11:35:15 +0100

    Specified code status

--- backend/index.js
+++ backend/index.js
@@ -16,15 +16,17 @@ apiServer.get("/", (request, response)=>{
 apiServer.get("/login", (request, response)=>{
     fs.readFile("users.json", (err, data)=>{
         if(err){
+            response.status(501);
             response.send("error: ", err);
         } else {
             var users = JSON.parse(data);
             var t1 = users.find(x => x.user==request.query.user);
             var t2 = users.find(x => x.pswd==request.query.pswd);
             if(t1==t2){
-                response.send("<h3>Checked</h3>")
-                response;
+                response.status(200);
+                response.send("<h3>Checked</h3>");
             } else {
+                response.status(500);
                 response.send("<h3>Wrong crdentials</h3>");
             }
         }
@@ -38,6 +40,7 @@ apiServer.get("/addUser",  (request, response)=>{
     fs.readFile("users.json", (err, data) => {
         if(err){
             console.log("Errore "+ err);
+            response.status(502);
             response.send("<body>ERROR<br>Cannot read user</body>");
         } else {
             var users = JSON.parse(data);
@@ -50,13 +53,16 @@ apiServer.get("/addUser",  (request, response)=>{
                 fs.writeFile("users.json", JSON.stringify(users), (err) =>{
                     if(err){
                         console.log("Errore "+ err);
+                        response.status(504);
                         response.send("<body>ERROR<br>Cannot save user</body>");
                     } else {
                         console.log(students);
+                        response.status(200);
                         response.send("<h3>Registered new user</h3>");
                     }
                 });
             } else {
+                response.status(400);
                 response.send("<h3>ERROR<br>User already exists</h3>");
             }
         }
