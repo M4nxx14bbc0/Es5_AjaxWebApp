752aa21b6880ccfbd9b1ece4d499b2ac71091778
Parra Rodriguez Manuel <parrarodriguez.manue@osdbsesto>
2022-03-03 11:13:00 +0100

    inserted method with mysql conn

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ var express = require("express");
 var apiServer = express();
 var fs = require("fs");
 var cors = require("cors");
+const mysql = require("mysql2");
 
 var port = 3000;
 var host = "localhost";
@@ -9,10 +10,30 @@ apiServer.listen(port, host, () => {
     console.log("Server running at http://%s:%d", host, port);
 });
 
+const conn = mysql.createConnection({
+    host: 'parrarodriguez.manue.tave.osdb.it',
+    user: 'c188_prm_5AI',
+    password: 'Az-52761',
+    database: 'c188_prm_5AI_2122'
+});
+
 apiServer.use(cors());
 
 apiServer.get("/", (request, response)=>{
     response.send("Ciao client sei in home");
+});
+
+apiServer.get("/mysql/login",  (request, response)=>{
+    console.log("Request: ", request.query);
+    conn.execute(
+        'SELECT * FROM c188_prm_5AI_2122.user WHERE mail="'+request.query.mail+'" AND pass="'+request.query.pass+'";',
+        (err, fields)=>{
+            console.error('Errore: ', err);
+            console.log('Successo: ', fields);
+        }
+    );
+    response.setHeader('Content-Type', 'application/json');
+    response.json({message: 'Successful!'}).sendStatus(200);
 });
 
 apiServer.get("/api/login", (request, response)=>{
